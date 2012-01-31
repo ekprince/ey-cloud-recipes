c14e9f0e6331ca6b12ebae1142a941e4173f0381
Ines Sombra <isombra@engineyard.com>
2012-01-30 16:27:59 -0800

    reverting version

--- cookbooks/postgresql9_contrib/attributes/recipe.rb
+++ cookbooks/postgresql9_contrib/attributes/recipe.rb
@@ -1,2 +1,3 @@
-postgres_version(node[:engineyard][:environment][:db_stack_name])
+# postgres_version(node[:engineyard][:environment][:db_stack_name])
+postgres_version("9.0")
 postgres_root("/db/postgresql/")
